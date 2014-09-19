6de8e5ee277b5e064ec63f5abe7ce53e633a7e01
Andrew Laiff <q.laiff@gmail.com>
2014-09-19 17:04:53 +0600

    Add lens functionality

--- src/lens.js
+++ src/lens.js
@@ -3,106 +3,89 @@
  *
  */
 /*global module:false, require:false, define:false,*/
-(function (root, factory) {
-    'use strict';
-    if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like enviroments that support module.exports, like Node.
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], function () {
-            return (root.lens = factory());
-        });
-    } else {
-        // Browser globals
-        root.lens = factory();
+'use strict';
+
+//генерация гетеров
+var get = function (prop) {
+    return function (item) {
+        return item[prop];
+    };
+};
+
+//генерация сетеров изменяемых структур
+var setMutable = function (prop) {
+    return function (value, item) {
+        item[prop] = value;
+        return item;
     }
-}(this, function () {
-    'use strict';
-
-    //генерация гетеров
-    var get = function (prop) {
-        return function (item) {
-            return item[prop];
-        };
+};
+//генерация сетеров для неизменяемых структур
+var setImmutable = function (prop) {
+    return function (value, item) {
+        var copy = properties(item)
+            .reduce(function (lst, next) {
+                lst[next] = item[next];
+                return lst;
+            }, {});
+        copy[prop] = value; //меняем на новое значение
+        return copy;
     };
+};
 
-    //генерация сетеров изменяемых структур
-    var setMutable = function (prop) {
-        return function (value, item) {
-            item[prop] = value;
-            return item;
+//возвращает список свойств объекта obj
+var properties = function (obj) {
+    var key, lst = [];
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            lst.push(key);
         }
-    };
-    //генерация сетеров для неизменяемых структур
-    var setImmutable = function (prop) {
-        return function (value, item) {
-            var props = properties(item), //получаем список всех свойств объекта
-                copy = props.reduce(function (lst, next) {
-                    lst[next] = item[next];
-                    return lst;
-                }, {});
-            copy[prop] = value; //меняем на новое значение
-            return copy;
-        };
-    };
+    }
+    return lst;
+};
 
-    //возвращает список свойств объекта obj
-    var properties = function (obj) {
-        var key, lst = [];
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                lst.push(key);
+var Lens = function (getter, setter) {
+    //Если передан 1 параметр, то это название свойства
+    if (arguments.length == 1) {
+        var property = arguments[0];
+        getter = get(property);
+        setter = setImmutable(property);
+    }
+
+    return {
+        modify: function (func, item) {
+            return setter(func(getter(item)), item);
+        },
+        compose: function (lens) {
+            return Lens(innerGetter, innerSetter);
+
+            function innerGetter(item) {
+                return lens.get(getter(item));
             }
-        }
-        return lst;
+
+            function innerSetter(value, item) {
+                var innerValue = lens.set(value, getter(item));
+                return setter(innerValue, item);
+            }
+        },
+        get: getter,
+        set: setter
     };
+};
 
-    var Lens = function (getter, setter) {
-        //Если передан 1 параметр, то это название свойства
-        if (arguments.length == 1) {
-            var property = arguments[0];
-            getter = get(property);
-            setter = setImmutable(property);
-        }
+var lens = function (cmd) {
+    var lenses = cmd.split('.').map(asOne(Lens));
 
-        return {
-            modify: function (func, item) {
-                return setter(func(getter(item)), item);
-            },
-            compose: function (lens) {
-                return Lens(innerGetter, innerSetter);
+    return lenses.reduce(function (lst, next) {
+        return lst.compose(next);
+    });
+};
 
-                function innerGetter(item) {
-                    return lens.get(getter(item));
-                }
-
-                function innerSetter(value, item) {
-                    var innerValue = lens.set(value, getter(item));
-                    return setter(innerValue, item);
-                }
-            },
-            get: getter,
-            set: setter
-        };
+//функция которая из переданной ей на вход функции делает такую,
+//которая игнорирует все переданные ей аргументы, кроме первого
+var asOne = function (func) {
+    return function (x) {
+        return func(x);
     };
+};
 
-    var lens = function (cmd) {
-        var lenses = cmd.split('.').map(asOne(Lens));
-
-        return lenses.reduce(function (lst, next) {
-            return lst.compose(next);
-        });
-    };
-
-    //функция которая из переданной ей на вход функции делает такую,
-    //которая игнорирует все переданные ей аргументы, кроме первого
-    var asOne = function (func) {
-        return function (x) {
-            return func(x);
-        };
-    };
-
-    return lens;
-}));
+module.exports = lens;
